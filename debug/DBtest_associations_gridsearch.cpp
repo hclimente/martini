d8f591c6b76840d6a6316cc66339536114e50a1a
Hector Climente <hclimente@gmail.com>
2017-03-15 18:40:14 +0100

    added files for debugging rscones

---
+++ debug/DBtest_associations_gridsearch.cpp
@@ -0,0 +1,5 @@
+#include "../src/test_associations_gridsearch.cpp"
+
+int main(){
+    test_associations_gridsearch(0, "/bioinfo/users/hcliment/easyGWASCore/data/testing/scones/skat/", 10, 1, 2);
+}
