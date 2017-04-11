68736e0c03f511185e6cce1ef57973d69e23037b
hclimente <hector.climente@curie.fr>
2017-04-11 16:49:45 +0200

    paths changed to fit new mac

--- debug/DBreadNetwork.cpp
+++ debug/DBreadNetwork.cpp
@@ -1,5 +1,12 @@
-#include "../src/readNetwork.cpp"
+#include "../src/readBio.cpp"
 
 int main(){
-    readNetwork("/bioinfo/users/hcliment/easyGWASCore/data/testing/scones/skat/network.txt");
+    readBio("/bioinfo/users/hcliment/easyGWASCore/data/testing/scones/skat/network.txt");
+
+    string wd = "/bioinfo/users/hcliment/easyGWASCore/data/testing/scones/skat/";
+    string pedBasename = wd + "genotype";
+    string phenoFile = wd + "phenotype.txt";
+    string netFile = wd + "network.txt";
+
+    readBio(pedBasename, phenoFile, netFile, 0, 0.05);
 }
