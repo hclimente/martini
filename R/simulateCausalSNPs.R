e33f28714d1c9a2f1e83f0cb6627d5f24dfe4650
Hector Climente <hclimente@gmail.com>
2017-05-10 14:31:49 +0200

    simulate causal snps
    pick some snps among the biggest clique on the network

---
+++ R/simulateCausalSNPs.R
@@ -0,0 +1,16 @@
+simulateCausalSNPs <- function(net, n) {
+
+  idx <- 1
+  
+  graph <- graph_from_adjacency_matrix(net, mode = "undirected", diag = FALSE)
+  cliques <- largest_cliques(graph)
+  cliqueIdx <- cliques[[idx]]
+  
+  # randomly select snps
+  causalSnpIdx <- sample(as.numeric(cliqueIdx), n)
+  
+  causalSnp <- rep(FALSE, nrow(net))
+  causalSnp[causalSnpIdx] <- TRUE
+  
+  return(causalSnp)
+}
