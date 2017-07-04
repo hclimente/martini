3f3dff3ae8c8765288c927ca03c7d22212e2dbd7
Hector Climente <hclimente@gmail.com>
2017-07-04 13:17:24 +0200

    added network calculation prototype functions

---
+++ R/getGSNet.R
@@ -0,0 +1,22 @@
+getGSNet <- function(map)  {
+  
+  map <- read.delim2(map, sep = " ", header = FALSE)
+  colnames(map) <- c("chr","gene","cm","pos")
+  map <- subset(map, select = c("chr","pos"))
+  map <- unique(map)
+  map <- map[with(map, order(chr,pos)),]
+  
+  gs <- by(map, map$chr, function(x){
+    chr <- unique(x$chr)
+    pos1 <- head(x$pos, n = length(x$pos) - 1)
+    pos2 <- tail(x$pos, n = length(x$pos) - 1)
+    data.frame(snp1 = paste(chr, pos1, sep = "_"), 
+               snp2 = paste(chr, pos2, sep = "_"))
+  })
+  gs <- do.call("rbind", gs)
+  gs <- graph_from_data_frame(gs, directed = FALSE)
+  gs_adj <- get.adjacency(gs, type="both", sparse = TRUE)
+  
+  return(gs_adj)
+  
+}
