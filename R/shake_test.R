cd5729294575809c45f43e060dbb107b1a8b3d42
hclimente <hclimente@gmail.com>
2017-07-11 16:39:09 +0200

    added some optimitation on shake_test

--- R/shake_test.R
+++ R/shake_test.R
@@ -1,34 +1,53 @@
 #' Calculate an empirical p-value for shake results.
 #' 
-#' @description Do a permutation-based test to assess the statistical significance of each of the clusters obtained through shake.
+#' @description Do a permutation-based test to assess the statistical significance of each of the clusters obtained through shake. For a cluster of size k, k interconnected SNPs are picked N times, and their joint association score is calculated to come up with an estimation of the distribution.
 #' 
 #' @param map Results from \code{shake}.
 #' @param net The same SNP network provided to \code{shake}.
-#' @param n Integer with the name of permutations.
-#' @return An empirical p-value for each of the SNP clusters.
+#' @param N Integer with the name of permutations.
+#' @return An empirical p-value for each of the SNP clusters. Please, note that the minimum possible p-value from an empirical distribution is set to 1/(N+1). Clusters composed by a single SNP will not have a empirical p-value.
 #' @export
-shake_test <- function(map, net, n = 100000) {
+shake_test <- function(map, net, N = 100000) {
   
   numSNPs <- vcount(net)
   selected <- subset(map, selected)
   
-  clusters <- by(map, map$cluster, function(k){
+  # calculate one ecdf for each cluster size
+  clusterSizes <- unique(table(selected$cluster))
+  names(clusterSizes) <- clusterSizes
+  
+  ecdfs <- lapply(clusterSizes, function(n) {
+    if (n > 1){
+      sampled_C <- lapply(1:N, function(i){
+        v <- sample(1:numSNPs, 1)
+        snpCluster_i <- random_walk(net, v, n)
+        map_cluster <- subset(map, snp %in% snpCluster_i)
+        sum(map_cluster$C)
+      })
+      sampled_C <- do.call("c", sampled_C)
+      
+      ecdf(sampled_C)
+    }
+  })
+  
+  
+  clusters <- by(selected, selected$cluster, function(k){
+    n <- nrow(k)
+    i <- unique(k$cluster)
     
-    snpCluster_C <- sum(k$C)
+    if (n > 1){
+      snpCluster_C <- sum(k$C)
+      p <- 1 - ecdfs[[as.character(n)]](snpCluster_C)
+    } else {
+      p <- NA
+    }
     
-    sampled_C <- lapply(1:n, function(i){
-      v <- sample(1:numSNPs, 1)
-      snpCluster_i <- random_walk(net, v, nrow(k))
-      map_cluster <- subset(map, snp %in% snpCluster_i)
-      sum(map_cluster$C)
-    })
-    sampled_C <- do.call("c", sampled_C)
-    
-    C_df <- ecdf(sampled_C)
-    data.frame(k = unique(k$cluster), ncomponents = nrow(k), p = C_df(snpCluster_C))
+    data.frame(k = i, ncomponents = n, p = p)
   })
   
   clusters <- do.call("rbind", clusters)
+  # minimum p-vaue = 1 / (N + 1)
+  clusters$p[clusters$p == 0] = 1 / (N + 1)
   
   return(clusters)
 
