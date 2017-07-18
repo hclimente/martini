eb6bcda64bcb2c55ae6ccf0fea0eda15d46fa2f7
hclimente <hclimente@gmail.com>
2017-07-18 20:47:59 +0200

    some fixes

--- R/plot_ideogram.R
+++ R/plot_ideogram.R
@@ -2,50 +2,54 @@
 #' 
 #' @description Create a circular ideogram of the \code{shake} results using the circlize package (Gu et al., 2014).
 #' 
-#' @param results Results from \code{shake}.
-#' @param net The same SNP network provided to \code{shake}.
+#' @param map Output from \code{shake}.
+#' @param net The SNP network provided to \code{shake}.
 #' @return A circular ideogram, including the manhattan plot, and the interactions between the selected SNPs.
 #' @references Gu, Z., Gu, L., Eils, R., Schlesner, M., & Brors, B. (2014). circlize Implements and enhances circular visualization in R. Bioinformatics (Oxford, England), 30(19), 2811–2. \url{https://doi.org/10.1093/bioinformatics/btu393}
 #' @export
-plot_ideogram <- function(results, net){
-  if (!requireNamespace("circlize", quietly = TRUE)) {
-    stop("circlize needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
+plot_ideogram <- function(map, net){
   
-  circos.initializeWithIdeogram()
+  if (!requireNamespace("circlize", quietly = TRUE))
+    stop("circlize needed for this function to work. Please install it.", call. = FALSE)
   
-  bed <- map2bed(results)
-  bed$C <- results$C
-  bed$selected <- results$selected
+  circlize::circos.initializeWithIdeogram()
+  
+  bed <- map2bed(map)
+  bed$C <- map$C
+  bed$selected <- map$selected
   # order to put the selected snps in fron in the plot
   bed <- bed[with(bed, order(selected)),]
   
-  circos.genomicTrackPlotRegion(bed, ylim = c(0, 1.1 * max(bed$C)), panel.fun = function(region, value, ...) {
-    # color according to selection/non-selection
-    col = ifelse(value[[2]], "orange", "gray70")
-    circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
-  }, track.height = 0.3)
+  circlize::circos.genomicTrackPlotRegion(bed, 
+                                          ylim = c(0, 1.1 * max(bed$C)), 
+                                          panel.fun = function(region, value, ...) {
+                                            # color according to selection/non-selection
+                                            col = ifelse(value[[2]], "orange", "gray70")
+                                            circlize::circos.genomicPoints(region, value, col = col, cex = 0.5, pch = 16)
+                                          }, track.height = 0.3)
   
   # create links
   # remove unselected SNPs from the graph
-  net <- delete_vertices(net, results$snp[! results$selected])
-  df <- get.data.frame(net)
+  df <- delete_vertices(net, as.character(map$snp[! map$selected])) %>%
+    get.data.frame %>%
+    subset(select = c("from", "to"))
   
-  region1 <- merge(df, results, by.x = "from", by.y = "snp", sort = F)
-  region1 <- map2bed(region1)
-  region2 <- merge(df, results, by.x = "to", by.y = "snp", sort = F)
-  region2 <- map2bed(region2)
+  region1 <- merge(df, map, by.x = "from", by.y = "snp", sort = F) %>%
+    subset(select = c("chr", "from", "cm", "pos")) %>%
+    map2bed
+  region2 <- merge(df, map, by.x = "to", by.y = "snp", sort = F) %>%
+    subset(select = c("chr", "to", "cm", "pos")) %>%
+    map2bed
   
-  circos.genomicLink(region1, region2)
+  circlize::circos.genomicLink(region1, region2)
   
-  circos.clear()
+  circlize::circos.clear()
   
 }
 
 map2bed <- function(map) {
   
-  bed <- subset(map, select = c("chr", "pos"))
+  bed <- map[,c(1,4)]
   colnames(bed) <- c("chr", "start")
   bed$chr <- paste0("chr", bed$chr)
   bed$chr <- ifelse(bed$chr == "chr23", "chrX", bed$chr)
