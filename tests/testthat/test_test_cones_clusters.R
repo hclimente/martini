9b6d18f3a188205f725d2d436c5b4f9229065985
hclimente <hclimente@gmail.com>
2017-07-19 17:44:17 +0200

    bugfix

--- tests/testthat/test_test_cones_clusters.R
+++ tests/testthat/test_test_cones_clusters.R
@@ -4,6 +4,6 @@ data(examplegwas)
 set.seed(0)
 
 test_that("the ordering of the map is not relevant", {
-  expect_equal(test_cones_clusters(map, net, N = 100), 
-               test_cones_clusters(map[sample(nrow(map)),], net, N = 100))
+  expect_equal(test_cones_clusters(examplegwas$map, examplegwas$net, N = 100), 
+               test_cones_clusters(examplegwas$map[sample(nrow(examplegwas$map)),], examplegwas$net, N = 100))
 })
