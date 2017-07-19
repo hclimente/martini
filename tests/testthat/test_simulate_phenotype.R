b9de5f3ef78516ffd5c702747e70fc160068f68a
hclimente <hclimente@gmail.com>
2017-07-19 17:32:17 +0200

    fixed examplegwas mentions

--- tests/testthat/test_simulate_phenotype.R
+++ tests/testthat/test_simulate_phenotype.R
@@ -2,23 +2,23 @@ library(martini)
 data(examplegwas)
 
 set.seed(0)
-causal <- sample(V(net), 5)
+causal <- sample(V(examplegwas$net), 5)
 eff <- rnorm(5)
 
 test_that("simulate_phenotype runs", {
-  expect_type(simulate_phenotype(gwas, causal, h2 = 1, effectSize = eff), "double")
-  expect_type(simulate_phenotype(gwas, causal, h2 = 1, effectSize = eff,
+  expect_type(simulate_phenotype(examplegwas$gwas, causal, h2 = 1, effectSize = eff), "double")
+  expect_type(simulate_phenotype(examplegwas$gwas, causal, h2 = 1, effectSize = eff,
                                  qualitative = T, ncases = 3000, ncontrols = 3000), "double")
 })
 
 # get 20 random snps and 20 random effect sizes
-causal <- sample(V(net), 20)
+causal <- sample(V(examplegwas$net), 20)
 eff <- rnorm(length(causal))
-X <- as(gwas$genotypes, "numeric")
-X_causal <- X[, gwas$map$snp.names %in% names(causal)]
+X <- as(examplegwas$gwas$genotypes, "numeric")
+X_causal <- X[, examplegwas$gwas$map$snp.names %in% names(causal)]
 
 # association between causal SNPs and the qualitative phenotype
-Y_ql <- simulate_phenotype(gwas, causal, h2 = 1, 
+Y_ql <- simulate_phenotype(examplegwas$gwas, causal, h2 = 1, 
                            effectSize = eff, 
                            qualitative = TRUE, ncases = 3000, ncontrols = 3000)
 ql_p <- apply(X_causal, 2, function(x){
@@ -28,7 +28,7 @@ ql_p <- apply(X_causal, 2, function(x){
 })
 
 # association between causal SNPs and the quantitative phenotype
-Y_qt <- simulate_phenotype(gwas, causal, h2 = 1, effectSize = eff)
+Y_qt <- simulate_phenotype(examplegwas$gwas, causal, h2 = 1, effectSize = eff)
 qt_p <- apply(X_causal, 2, function(x){
   wilcox.test(Y_qt, x)$p.value
 })
