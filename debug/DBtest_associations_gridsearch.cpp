434ce71c4e1183756fe8c3e7dcb0817764c87b10
hclimente <hclimente@gmail.com>
2017-08-22 17:28:50 +0200

    removed debug folder, as we can debug with lldb

--- debug/DBtest_associations_gridsearch.cpp
+++
@@ -1,9 +0,0 @@
-#include "../src/runScones.cpp"
-
-int main(){
-    string wd = "/bioinfo/users/hcliment/easyGWASCore/data/testing/scones/skat/";
-    string pedBasename = wd + "genotype";
-    string phenoFile = wd + "phenotype.txt";
-    string netFile = wd + "network.txt";
-#runScones(0, , 10, 1, 2);
-}
