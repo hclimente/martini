574f257b2439dbc4e5add35620acee824ef5d0eb
hclimente <hclimente@gmail.com>
2017-10-04 14:15:09 +0200

    updated to use modules instead of clusters

---
+++ tests/testthat/test_test_cones_modules.R
@@ -0,0 +1,9 @@
+library(martini)
+data(examplegwas)
+
+set.seed(0)
+
+test_that("the ordering of the map is not relevant", {
+  expect_equal(test_cones_modules(examplegwas$cones, examplegwas$net, N = 100), 
+               test_cones_modules(examplegwas$cones[sample(nrow(examplegwas$cones)),], examplegwas$net, N = 100))
+})
