92c929deaf741e154b1daea907751b726c7c52a0
hclimente <hclimente@gmail.com>
2017-11-03 14:57:54 +0100

    added epistasis calculation functions

---
+++ R/epistasis.R
@@ -0,0 +1,45 @@
+#' Measure epistasis between SNPs
+#' 
+#' @description Measure epistasis between SNPs along the edges of net.
+#' 
+#' @param gwas A GWAS experiment.
+#' @param net A network of SNPs.
+#' @return A dataframe with the chi-squared score for each pair of tested SNPs.
+#' @importFrom igraph as_data_frame
+#' @importFrom BiocParallel bpmapply
+#' @export
+measure_epistasis <- function(gwas, net) {
+  
+  edges <- as_data_frame(net, what = "edges")
+  colnames(edges) <- c("snp1", "snp2")
+  
+  X <- as(gwas$genotypes, "numeric")
+  colnames(X) <- as.character(gwas$map[,2])
+  
+  edges$score <- bpmapply(function(x, y) {
+    chisq.test(X[,x], X[,y])$statistic
+  }, edges$snp1, edges$snp2)
+  
+  return(edges)
+  
+}
+
+#' Incorporate epistasis into the network
+#' 
+#' @description Incorporate epistatic interactions in the network.
+#' 
+#' @param net A network of SNPs.
+#' @param scores A dataframe with pairs of SNPs and the measure of the epistatic effect.
+#' @return A network where the weight of the edges reflect the strength of the epistatic interaction.
+#' @importFrom igraph E set_edge_attr %>%
+#' @export
+weight_epistasis_network <- function(net, scores) {
+  
+  colnames(scores) <- c("snp1", "snp2", "score")
+  snps <- subset(scores, select = c("snp1", "snp2")) %>% t %>% c
+  
+  net <- set_edge_attr(net, "weight", index = E(net, P=snps), value = scores$score)
+  
+  return(net)
+  
+}
---
+++ tests/testthat/test_measure_epistasis.R
@@ -0,0 +1,32 @@
+library(martini)
+data(examplegwas)
+
+subnet <- igraph::delete_edges(examplegwas$net, sample(igraph::E(examplegwas$net), 39564))
+epistasis <- measure_epistasis(examplegwas$gwas, subnet)
+edges <- igraph::as_data_frame(subnet, what = "edges")
+colnames(edges) <- c("snp1", "snp2")
+X <- as(examplegwas$gwas$genotypes, "numeric")
+colnames(X) <- as.character(examplegwas$gwas$map[,2])
+
+test_that("we calculate epistasis among the expected edges", {
+  expect_equal(dim(epistasis), c(10,3))
+  expect_equal(dim(merge(epistasis, edges)), c(10,3))
+  expect_equal(colnames(epistasis), c("snp1", "snp2", "score"))
+})
+
+test_that("epistasis is calculated correctly", {
+  expect_equal(subset(epistasis, snp1 == edges$snp1[1] & snp2 == edges$snp2[1])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[1]], X[,edges$snp2[1]])$statistic ))
+  expect_equal(subset(epistasis, snp1 == edges$snp1[2] & snp2 == edges$snp2[2])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[2]], X[,edges$snp2[2]])$statistic ))
+  expect_equal(subset(epistasis, snp1 == edges$snp1[3] & snp2 == edges$snp2[3])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[3]], X[,edges$snp2[3]])$statistic ))
+  expect_equal(subset(epistasis, snp1 == edges$snp1[4] & snp2 == edges$snp2[4])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[4]], X[,edges$snp2[4]])$statistic ))
+  expect_equal(subset(epistasis, snp1 == edges$snp1[5] & snp2 == edges$snp2[5])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[5]], X[,edges$snp2[5]])$statistic ))
+  expect_equal(subset(epistasis, snp1 == edges$snp1[6] & snp2 == edges$snp2[6])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[6]], X[,edges$snp2[6]])$statistic ))
+  expect_equal(subset(epistasis, snp1 == edges$snp1[7] & snp2 == edges$snp2[7])$score, 
+               as.numeric( chisq.test(X[,edges$snp1[7]], X[,edges$snp2[7]])$statistic ))
+})
