e68f753548e8a7879f23adc4523b6151446b11c8
Héctor Climente-González <hclimente@gmail.com>
2021-06-24 13:09:53 +0200

    improved documentation and code readability

--- src/mincut.cpp
+++ src/mincut.cpp
@@ -9,6 +9,14 @@
 
 using namespace Rcpp;
 
+//' Maxflow algorithm
+//' 
+//' @description Run the maxflow algorithm.
+//' @param A A matrix with two columns. The first column contains the edges to
+//' the source; the second, the edges to the sink.
+//' @param W A sparse matrix with the connectivity.
+//' @return A list with vector indicating if the feature was selected and the 
+//' objective score.
 // [[Rcpp::export]]
 LogicalVector maxflow(Eigen::MatrixXd const &A,
                       Eigen::SparseMatrix<double,Eigen::ColMajor> const &W) {
@@ -22,21 +30,21 @@ LogicalVector maxflow(Eigen::MatrixXd const &A,
   // initialize nodes
   g->add_node(selected.length());
   
-  //traverse the sparse adjacency matrix A
+  // add edge weights from the original graph W
   for(long long k=0; k<W.outerSize(); k++) {
     for(Eigen::SparseMatrix<double>::InnerIterator it(W,k); it; ++it) {
       g->add_edge(it.row(), it.col(), it.value(), 0.0);
     }
   }
   
-  // traverse the T matrix
+  // add edges to the source and the sink from A
   for(long long i = 0; i < 2; i++) {
     for(long long k = 0; k < A.rows(); k++) {
-      if(i==0) {
+      if(i==0) { // source
         if(A(k,i) != 0) {
           g->add_tweights(k, A(k,i), 0.0);
         }
-      } else {
+      } else { // sink
         if(A(k,i) != 0) {
           g->add_tweights(k, 0.0, A(k,i));
         }
@@ -55,6 +63,7 @@ LogicalVector maxflow(Eigen::MatrixXd const &A,
   delete g;
   
   return(selected);
+  
 }
 
 //' Min-cut algorithm
@@ -75,19 +84,19 @@ LogicalVector mincut_c(Eigen::VectorXd c, double eta, double lambda,
   
   Eigen::VectorXd c_t = c.array() - eta;
   
-  // Add source and sink
-  // Matrix A containing As and At
+  // add source and sink
+  // matrix A containing As (edges to source) and At (edges to sink)
   Eigen::MatrixXd A(n_features, 2);
   // connect positive c values to sink
   Eigen::VectorXd pos_c = (c_t.array() <= 0).select(0, c_t);
   // connect negative c values to source
-  Eigen::VectorXd neg_c = - c_t;
+  Eigen::VectorXd neg_c = -c_t;
   neg_c = (c_t.array() > 0).select(0, neg_c);
-  //Store data
+  // store data
   A.col(0) = neg_c;
   A.col(1) = pos_c;
   
-  //compute maxflow
+  // compute maxflow
   LogicalVector selected = maxflow(A, W);
   return(selected);
   
